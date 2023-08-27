91ad14a70e5f906da4a991dc0e0f57f21aad6917
Jalil David Salamé Messina <jalil.salame@gmail.com>
2023-08-27 11:36:13 +0200

    feat: Enable more LSPs

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,13 +1,17 @@
 {
   enable = true;
+  servers.bashls.enable = true;
   servers.clangd.enable = true;
   servers.html.enable = true;
-  # servers.lua-language-server.enable = true;
+  servers.lua-language-server.enable = true;
   servers.jsonls.enable = true;
+  servers.nil.enable = true;
+  servers.pyright.enable = true;
+  servers.rnix.enable = true;
   servers.ruff-lsp.enable = true;
   # servers.rust-analyzer.enable = true;
   # servers.serve_d.enable = true;
-  servers.statix.enable = true;
+  # servers.statix.enable = true;
   servers.taplo.enable = true;
   servers.typst-lsp.enable = true;
   # servers.vimls.enable = true;
