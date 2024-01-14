38aace6c5897ba5bf6218fd4389b7acf71304258
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-01-14 22:15:09 +0100

    fix: Tidy up code

--- flake.nix
+++ flake.nix
@@ -3,53 +3,33 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  # inputs.nixneovim.url = "github:jalil-salame/NixNeovim/remove-events-enum";
   inputs.nixneovim.url = "github:NixNeovim/NixNeovim";
   inputs.nixneovim.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixneovim.inputs.flake-utils.follows = "flake-utils";
   inputs.nixneovim.inputs.home-manager.follows = "home-manager";
 
-  inputs.nixneovimplugins.url = "github:jalil-salame/NixNeovimPlugins/idris2-plugin";
-  inputs.nixneovimplugins.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixneovimplugins.inputs.flake-utils.follows = "flake-utils";
-  inputs.nixneovim.inputs.nixneovimplugins.follows = "nixneovimplugins";
-
   inputs.neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
   inputs.neovim-nightly.inputs.nixpkgs.follows = "nixpkgs";
 
-  # To ensure no duplication of inputs (you'll probably use home-manager)
   inputs.home-manager.url = "github:nix-community/home-manager";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    nixneovim,
-    neovim-nightly,
-    ...
-  }: let
-    nvim-config = import ./config;
-    overlays = {
-      nixneovim = nixneovim.overlays.default;
-      neovim-nightly = neovim-nightly.overlay;
-    };
-  in
+  outputs = { nixpkgs, flake-utils, nixneovim, neovim-nightly, ... }:
+    let
+      nvim-config = import ./config;
+      overlays = {
+        nixneovim = nixneovim.overlays.default;
+        neovim-nightly = neovim-nightly.overlay;
+      };
+    in
     {
       nixosModules = {
         inherit nvim-config;
         nixneovim = nixneovim.nixosModules.homeManager;
         nixneovim-23-05 = nixneovim.nixosModules.homeManager-23-05;
-        default = {};
+        default = { };
       };
       inherit overlays;
     }
-    // flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in {
-        formatter = pkgs.alejandra;
-      }
-    );
+    // flake-utils.lib.eachDefaultSystem (system: let pkgs = import nixpkgs { inherit system; }; in { formatter = pkgs.nixpkgs-fmt; });
 }
