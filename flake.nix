63aef38201f85e53108eca092cb8383dc3b02171
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-01-15 17:56:42 +0100

    fix(flake): Move to flakehub dependecies where possible

--- flake.nix
+++ flake.nix
@@ -1,32 +1,39 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
   inputs.nixneovim.url = "github:NixNeovim/NixNeovim";
   inputs.nixneovim.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixneovim.inputs.flake-utils.follows = "flake-utils";
   inputs.nixneovim.inputs.home-manager.follows = "home-manager";
+
+  inputs.home-manager.url = "https://flakehub.com/f/nix-community/home-manager/0.1.*.tar.gz";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
   inputs.neovim-nightly.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.home-manager.url = "github:nix-community/home-manager";
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
 
-  outputs = { nixpkgs, flake-utils, nixneovim, neovim-nightly, ... }:
+  outputs = { nixpkgs, flake-schemas, nixneovim, neovim-nightly, ... }:
     let
-      nvim-config = import ./config;
+      # Helpers for producing system-specific outputs
+      inherit (nixpkgs) lib;
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+      forEachSupportedSystem = f: lib.genAttrs supportedSystems (system: f (import nixpkgs { inherit system; }));
       # Merge both overlays together
       overlays.default = final: prev: (nixneovim.overlays.default final prev) // (neovim-nightly.overlays.default final prev);
+      # Create module
+      nvim-config.imports = [ ./config nixneovim.nixosModules.homeManager ];
     in
     {
+      # Schemas tell Nix about the structure of your flake's outputs
+      inherit (flake-schemas) schemas;
       nixosModules = {
         inherit nvim-config;
-        default = { imports = [ nvim-config nixneovim.nixosModules.homeManager ]; };
+        default = nvim-config;
       };
 
+      formatter = forEachSupportedSystem (pkgs: pkgs.nixpkgs-fmt);
+
       inherit overlays;
-    }
-    // flake-utils.lib.eachDefaultSystem (system: let pkgs = import nixpkgs { inherit system; }; in { formatter = pkgs.nixpkgs-fmt; });
+    };
 }
